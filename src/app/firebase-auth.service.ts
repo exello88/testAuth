5a1b6eece1c1ef2009baab28c90ec701bde2a33e
ex <orlovichlesha1@gmail.com>
2024-08-26 12:10:09 +0300

    config update

--- src/app/firebase-auth.service.ts
+++ src/app/firebase-auth.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { initializeApp } from 'firebase/app';
 import { getAuth, Auth } from 'firebase/auth';
+
 const firebaseConfig = {
-  apiKey: "AlzaSyC7CWVs00HqWNhmaGFafvtP5nZ2HOF-plJ",
+  apiKey: "AIzaSyC7CWVsfjoHqWNhmaGFafvtP5nZ2HOF-pU",
   authDomain: "moodystore2-15929.firebaseapp.com",
+  databaseURL: "https://moodystore2-15929-default-rtdb.firebaseio.com",
   projectId: "moodystore2-15929",
   storageBucket: "moodystore2-15929.appspot.com",
   messagingSenderId: "761012315",
+  appId: "1:761012315:web:027153629ce74d372a1f7e",
+  measurementId: "G-4RE3T2GBD7"
 };
 
 @Injectable({
